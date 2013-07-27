5c75fa234a5c64de184a9d781507c4cd67a4de2b
Manic <basbalmanic@gmail.com>
2013-07-27 14:32:49 -0700

    Made spider boss

---
+++ game/lib/game/entities/enemySpiderBoss.js
@@ -0,0 +1,13 @@
+ig.module(
+	'game.entities.enemySpiderBoss'
+).requires(
+	'game.entities.enemySpider'
+).defines(function() {
+	EntityEnemySpiderBoss = EntityEnemySpider.extend({
+		size:{x:256,y:256},
+		
+		health:100,
+		
+		animSheet: new ig.AnimationSheet( 'media/spiderBoss.png', 256,256 ),		
+	});
+});
