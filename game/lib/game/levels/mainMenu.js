aceb88329507f8aa7d4e4bdec8d052ee7f527ecc
Manic <basbalmanic@gmail.com>
2013-08-15 03:21:59 -0700

    Added menu graphic

--- game/lib/game/levels/mainMenu.js
+++ game/lib/game/levels/mainMenu.js
@@ -1,5 +1,6 @@
 ig.module( 'game.levels.mainMenu' )
 .requires( 'impact.image','game.entities.mainMenu' )
 .defines(function(){
-LevelMainMenu=/*JSON[*/{"entities":[{"type":"EntityMainMenu","x":272,"y":136}],"layer":[]}/*]JSON*/;
+LevelMainMenu=/*JSON[*/{"entities":[{"type":"EntityMainMenu","x":272,"y":136}],"layer":[{"name":"bgLayer","width":13,"height":10,"linkWithCollision":false,"visible":1,"tilesetName":"media/menu.jpg","repeat":false,"preRender":true,"distance":"1","tilesize":64,"foreground":false,"data":[[33,34,35,36,37,38,39,39,39,39,39,40,41],[48,49,50,51,52,53,54,54,54,54,54,55,56],[63,64,65,66,67,68,69,69,69,69,69,70,71],[78,79,80,81,82,83,84,84,84,84,84,85,86],[93,94,95,96,97,98,99,99,99,99,99,100,101],[108,109,110,111,112,113,114,114,114,114,114,115,116],[123,124,125,126,127,128,129,129,129,129,129,130,131],[138,139,140,141,142,143,144,144,144,144,144,145,146],[153,154,155,156,157,158,159,159,159,159,159,160,161],[168,169,170,171,172,173,174,174,174,174,174,175,176]]}]}/*]JSON*/;
+LevelMainMenuResources=[new ig.Image('media/menu.jpg')];
 });
